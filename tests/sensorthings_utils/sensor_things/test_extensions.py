d370c87e8c48f7467cdb76530a2d2726b018bea5
Justin Schembri <j.schembri@tudelft.nl>
2026-04-17 12:55:55 +0200

    some generated tests

--- tests/sensorthings_utils/sensor_things/test_extensions.py
+++ tests/sensorthings_utils/sensor_things/test_extensions.py
@@ -2,9 +2,13 @@
 
 # standard
 from pathlib import Path
+from copy import deepcopy
 
+# external
+import yaml
+import pytest
 # internal
-from sensorthings_utils.sensor_things.extensions import SensorConfig
+from sensorthings_utils.sensor_things.extensions import SensorConfig, SensorArrangement
 
 TEST_DATA_DIR = Path(__file__).parent / "data"
 GOOD_CONFIG_FILE = TEST_DATA_DIR / "valid_sensor_config.yaml"
@@ -22,3 +26,40 @@ class TestSensorConfig:
     def test_empty_iot_link_invalid(self):
         bad_config = SensorConfig(EMPTY_IOT_LINK_CONFIG_FILE)
         assert bad_config.is_valid is False
+
+    def test_ingestion_sets_model_and_name_metadata(self):
+        good_config = SensorConfig(GOOD_CONFIG_FILE)
+        assert good_config.model.value == "netatmo.nws03"
+        assert good_config.name == "sensor-001"
+
+    def test_ingestion_resolves_links_to_object_refs(self):
+        arrangement = SensorArrangement(SensorConfig(GOOD_CONFIG_FILE))
+
+        sensor = arrangement.get("Sensor", instance="sensor-001")
+        datastream = arrangement.get("Datastream", instance="temperature_indoor")
+        thing = arrangement.get("Thing", instance="room-120")
+        location = arrangement.get("Location", instance="loc-120")
+        observed_property = arrangement.get("ObservedProperty", instance="indoor_temperature")
+
+        assert sensor.iot_links["datastreams"][0] is datastream
+        assert datastream.iot_links["sensors"][0] is sensor
+        assert datastream.iot_links["things"][0] is thing
+        assert datastream.iot_links["observedProperties"][0] is observed_property
+        assert thing.iot_links["locations"][0] is location
+        assert location.iot_links["things"][0] is thing
+
+    def test_guardrail_missing_link_target_raises_keyerror(self, tmp_path):
+        with open(GOOD_CONFIG_FILE, "r") as f:
+            config_data = yaml.safe_load(f)
+
+        bad_data = deepcopy(config_data)
+        bad_data["sensors"]["netatmo.nws03"]["iot_links"]["datastreams"] = [
+            "does_not_exist"
+        ]
+
+        bad_config_path = tmp_path / "bad_missing_link_target.yaml"
+        with open(bad_config_path, "w") as f:
+            yaml.safe_dump(bad_data, f, sort_keys=False)
+
+        with pytest.raises(KeyError):
+            SensorArrangement(SensorConfig(bad_config_path))
