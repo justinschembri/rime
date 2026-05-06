070c4b11e5302dbaa7ad408e60d495516fa88724
Justin Schembri <j.schembri@tudelft.nl>
2026-05-06 17:21:49 +0200

    dead import

--- src/rime/providers/__init__.py
+++ src/rime/providers/__init__.py
@@ -1,8 +1,7 @@
 """Concrete sensor application providers."""
 
-from .generic_http import GenericHTTPProvider
 from .netatmo import NetatmoProvider
 from .registry import PROVIDER_REGISTRY
 from .tts import TTSProvider
 
-__all__ = ["GenericHTTPProvider", "NetatmoProvider", "TTSProvider", "PROVIDER_REGISTRY"]
+__all__ = ["NetatmoProvider", "TTSProvider", "PROVIDER_REGISTRY"]
